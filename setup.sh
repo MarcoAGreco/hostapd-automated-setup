6cd21118d9ff425859620429a70965613663cdea
MarcoAGreco <marcoandreagreco@gmail.com>
2020-10-15 19:54:32 +0200

    Update dependencies

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ if (( $# != 3 )); then
     exit -1
 fi
 
+if ($1 == '--help'); then
+    echo "[-] Syntax: recon.sh [interface] [SSID] [PASSPHRASE]"
+    echo '[-] Example: recon.sh wlan0 my_ssid my_passhphrase'
+    exit 0
+fi
 
 echo "[INFO] Creating access point through $1"
 
@@ -27,8 +32,56 @@ then
     echo "[ERROR] Error during hostapd installation. Aborting."
     exit
 else
-    echo "[INFO] hostapd successfully installed. Starting AP configuration."
+    echo "[INFO] hostapd successfully installed."
 fi
+
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+
+
+sudo apt install -y dnsmasq
+
+if [[ $? > 0 ]]
+then
+    echo "[ERROR] Error during dnsmasq installation. Aborting."
+    exit
+else
+    echo "[INFO] dnsmasq successfully installed."
+fi
+
+sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
+
+if [[ $? > 0 ]]
+then
+    echo "[ERROR] Error during iptables/netfilter installation. Aborting."
+    exit
+else
+    echo "[INFO] iptables/netfilter successfully installed."
+fi
+
+
+echo "interface $1
+    static ip_address=192.168.4.1/24
+    nohook wpa_supplicant" >> ./conf/dhcpcd.conf
+
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+# Enable ipv4 forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+
+sudo netfilter-persistent save
+
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+echo "
+interface=$1 # Listening interface
+dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
+                # Pool of IP addresses served via DHCP
+domain=wlan     # Local wireless DNS domain
+address=/gw.wlan/192.168.4.1
+                # Alias for this router
+"
+
+sudo rfkill unblock wlan
 
 # Setup hostapd configuration file
 sudo touch ./conf/hostapd.conf
@@ -49,8 +102,5 @@ rsn_pairwise=CCMP" > ./conf/hostapd.conf
 
 cp ./conf/hostapd.conf /etc/hostapd/hostapd.conf
 cp /.conf/hostapd /etc/default/hostapd
-
-# Enable ipv4 forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
 
 sudo hostapd /etc/hostapd/hostapd.conf
